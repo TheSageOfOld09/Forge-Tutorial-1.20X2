178c2f48a950caf16475665e1cc9d23450938ec7
TheSageOfOld <jdfun09@gmail.com>
2024-08-13 16:51:14 -0700

    update to code

--- src/main/java/net/joshuad/trail_run_1/Trial_Run_1.java
+++ src/main/java/net/joshuad/trail_run_1/Trial_Run_1.java
@@ -1,6 +1,8 @@
 package net.joshuad.trail_run_1;
 
 import com.mojang.logging.LogUtils;
+import net.joshuad.trail_run_1.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -22,6 +24,8 @@ public class Trial_Run_1 {
     public Trial_Run_1() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
+        ModItems.register(modEventBus);
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -35,7 +39,9 @@ public class Trial_Run_1 {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.RUBY);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
--- src/main/java/net/joshuad/trail_run_1/item/ModItems.java
+++ src/main/java/net/joshuad/trail_run_1/item/ModItems.java
@@ -1,4 +1,21 @@
 package net.joshuad.trail_run_1.item;
 
+import net.joshuad.trail_run_1.Trial_Run_1;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModItems {
+    public static  final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Trial_Run_1.MOD_ID);
+
+    public static final RegistryObject<Item> RUBY = ITEMS.register("ruby",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
 }
